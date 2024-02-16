8723dacc3fbf5c76ffae11a58d8e122862fa39bb
unknown <rostik057@gmail.com>
2024-02-16 13:59:23 +0300

    !fix

--- resources/views/pages/admin/post/create.blade.php
+++ resources/views/pages/admin/post/create.blade.php
@@ -1,5 +1,21 @@
 @extends('layout.admin.index')
 
 @section('html')
-    
+<form class="admin-form" action="">
+    <div class="admin-form__inputs">
+        <label class="admin-label">
+            <span class="admin-label__title">Поле</span>
+            <input class="admin-input admin-label__input" type="text">
+        </label>
+        <label class="admin-label">
+            <span class="admin-label__title">Поле</span>
+            <input class="admin-input admin-label__input" type="text">
+        </label>
+        <label class="admin-label">
+            <span class="admin-label__title">Поле</span>
+            <input class="admin-input admin-label__input" type="text">
+        </label>
+    </div>
+    <button class="admin-btn admin-form__btn">Создать</button>
+</form>
 @endsection
