d25a031d03815106b7c8ff1cdc41d6bcb3c15d6e
unknown <rostik057@gmail.com>
2024-02-19 13:17:07 +0300

    !fix

--- app/Http/Controllers/OrderingController.php
+++ app/Http/Controllers/OrderingController.php
@@ -19,7 +19,7 @@ class OrderingController extends ApiController
     }
 
     public function store(Request $request) {
-        if (auth()->user()?->product_users()->count() == 0) return AccessUtil::errorMessage('Cart empty', 400);
+        if (auth()->user()?->product_users()->whereNull('ordering_id')->count() == 0) return AccessUtil::errorMessage('Cart empty', 400);
 
         return parent::store($request);
     }
