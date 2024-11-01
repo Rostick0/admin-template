4cef3dc6ce01a757af94a214f0d2e84ca8b0dfee
Rostislav <rostik057@gmail.com>
2024-11-01 11:25:43 +0300

    !wip profile

--- app/Http/Controllers/UserStatisticController.php
+++ app/Http/Controllers/UserStatisticController.php
@@ -7,6 +7,7 @@ use App\Models\OrderingProduct;
 use App\Models\Review;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Rostislav\LaravelFilters\Filter;
 
 class UserStatisticController extends Controller
@@ -19,8 +20,13 @@ class UserStatisticController extends Controller
         $data = [
             'total_sum' => Filter::query($request, new Ordering, [], [$user_id])->sum('price'),
             'comments_sum' => Filter::query($request, new Review, [], [$user_id])->count(),
-            'buy_count' => Filter::query($request, new OrderingProduct, [], [[...$user_id, 'ordering']])->count(),
-            // 'a' => Filter::query($request, new Ordering, [], [$user_id])->ordering_product,
+            'buy_count' => Filter::query($request, new OrderingProduct, [], [[...$user_id, 'ordering']])
+                ->distinct('product_id')
+                ->count(),
+            'doesnt_have_review' => Filter::query($request, new OrderingProduct, [], [[...$user_id, 'ordering'],])
+                ->doesntHave('product.my_review')
+                ->distinct('product_id')
+                ->count()
         ];
 
         return new JsonResponse([
