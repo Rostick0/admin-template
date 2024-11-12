164d7a883c94d3144881cb81e1c2a1e54592ba7c
Rostislav <rostik057@gmail.com>
2024-11-12 23:57:46 +0300

    !fix valid download uploader

--- app/Http/Requests/Uploader/DownloadUploaderRequest.php
+++ app/Http/Requests/Uploader/DownloadUploaderRequest.php
@@ -22,6 +22,7 @@ class DownloadUploaderRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'model' => 'required|in:Product',
             'type' => 'required|in:Csv,Json,Xml'
         ];
     }
